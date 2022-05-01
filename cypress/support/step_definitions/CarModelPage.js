7d383ce819739654c23610d9635f1bf8dfcb01cb
dgerike <dennis.gerike+github@gmail.com>
2022-05-01 11:55:51 +0200

    E2E-18 adding more test steps; improving the existing test steps

--- cypress/support/step_definitions/CarModelPage.js
+++ cypress/support/step_definitions/CarModelPage.js
@@ -12,12 +12,24 @@ Then('a timeline for this car model should be displayed', () => {
     cy.get('h2')
         .contains('Timeline')
         .should('be.visible')
+
+    cy.get('h2')
+        .contains('Timeline')
+        .find('+ div')
+        .contains('Car Model')
+        .should('be.visible')
 })
 
 Then('a list of car model variants for this car model should be displayed', () => {
     cy.get('h2')
         .contains('Car Model Variants')
         .should('be.visible')
+
+    cy.get('h2')
+        .contains('Car Model Variants')
+        .find('+ div .card-node')
+        .should('be.visible')
+        .should('have.length.greaterThan', 1)
 })
 
 Then('a description for this car model should be displayed', () => {
@@ -27,10 +39,40 @@ Then('a description for this car model should be displayed', () => {
                 .contains(carModel.name)
                 .should('be.visible')
         })
+
+    cy.get('h1')
+        .parent()
+        .find('+ p')
+        .should('be.visible')
 })
 
 Then('a fact sheet for this car model should be displayed', () => {
     cy.get('h2')
         .contains('Fact Sheet')
         .should('be.visible')
+
+    cy.get('h2')
+        .contains('Fact Sheet')
+        .find('+ div dt, + div dl')
+        .should('be.visible')
+})
+
+Then('photos for this car model should be displayed', () => {
+    cy.get('article img')
+        .should('be.visible')
+
+    cy.get('.summary-box-images img')
+        .should('be.visible')
+})
+
+Then('exactly one brand should be connected to this car model', () => {
+    cy.get('h2')
+        .contains('Car Brands')
+        .should('be.visible')
+
+    cy.get('h2')
+        .contains('Car Brands')
+        .find('+ div .card-node')
+        .should('be.visible')
+        .should('have.length', 1)
 })
