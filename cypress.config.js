0e8ec0f240a89566dff4e9f80a5782c23fd2753f
dgerike <dennis.gerike+github@gmail.com>
2022-08-31 11:53:58 +0200

    #21 updating the configurations, so the preprocessor works again

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,21 @@
 const { defineConfig } = require("cypress");
-const cucumber = require('cypress-cucumber-preprocessor').default;
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+async function setupNodeEvents(on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on(
+      "file:preprocessor",
+      createBundler({
+        plugins: [createEsbuildPlugin.default(config)],
+      })
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 module.exports = defineConfig({
   projectId: "zoms1v",
@@ -17,8 +33,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://more-cars.net',
     specPattern: '**/*.feature',
-    setupNodeEvents(on, config) {
-      on('file:preprocessor', cucumber());
-    },
+    setupNodeEvents,
   },
 });
