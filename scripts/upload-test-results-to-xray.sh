87bef99daed148b91ddbec9a3c97ee67ec63d30a
dgerike <dennis.gerike@gmail.com>
2023-02-23 14:48:13 +0100

    #26 fixing the test report uploader (Jira/Xray)

--- scripts/upload-test-results-to-xray.sh
+++ scripts/upload-test-results-to-xray.sh
@@ -13,27 +13,23 @@ if [ -z "${XRAY_API_CLIENT_SECRET}" ]; then
 fi
 
 # ...so we can request a temporary API token
-XRAY_API_TOKEN=`${SCRIPTS_DIR}/libs/obtain-xray-api-token.sh`
+XRAY_API_TOKEN=`"${SCRIPTS_DIR}"/libs/obtain-xray-api-token.sh`
 
 # creating meta file
-node ${SCRIPTS_DIR}/libs/createMetaInfoFile.mjs;
-metaInfoFile=${SCRIPTS_DIR}/temp/meta-info.json
+node "${SCRIPTS_DIR}"/libs/createMetaInfoFile.mjs
+metaInfoFile="${SCRIPTS_DIR}"/temp/meta-info.json
 
 # uploading each test result one by one
-for resultFile in ${SCRIPTS_DIR}/../cypress/reports/*.json; do
-    echo "Uploading: $resultFile"
+resultFile="${SCRIPTS_DIR}"/../cypress/reports/json/test-results.json
+echo "Uploading: $resultFile"
 
-    RESPONSE=`curl -# --fail --retry 6 \
-        -X POST 'https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart' \
-        -H "Authorization: Bearer $XRAY_API_TOKEN" \
-        -H "Content-Type: multipart/form-data" \
-        -F results="@${resultFile}" \
-        -F info="@${metaInfoFile}"`
+RESPONSE=`curl -# --fail --retry 6 \
+    -X POST 'https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart' \
+    -H "Authorization: Bearer $XRAY_API_TOKEN" \
+    -H "Content-Type: multipart/form-data" \
+    -F results="@${resultFile}" \
+    -F info="@${metaInfoFile}"`
 
-    JIRA_ISSUE_ID=`node ${SCRIPTS_DIR}/libs/extractJiraIssueId.mjs $RESPONSE`;
-    echo "Test execution results uploaded to https://more-cars.atlassian.net/browse/$JIRA_ISSUE_ID"
-    echo
-
-    # waiting a little bit to not overwhelm the Xray API (aka: not running into the API limit)
-    sleep 0.5s
-done
+JIRA_ISSUE_ID=`node "${SCRIPTS_DIR}"/libs/extractJiraIssueId.mjs $RESPONSE`;
+echo "Test execution results uploaded to https://more-cars.atlassian.net/browse/$JIRA_ISSUE_ID"
+echo
